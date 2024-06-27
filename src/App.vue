9aeee38d7b7268c3eeb89b4a045bb6e9149337d1
“elisejonkers” <elisejonkers18@hotmail.com>
2024-06-27 14:36:28 +0200

    chore: added TheHeader and BaseCard component with styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-<stored-resources :resources="storedResources"></stored-resources>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue'
+import TheHeader from './components/layouts/TheHeader.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
